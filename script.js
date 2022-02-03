003b463491506c45b8bd2da533272f43129448f6
Misaki <tshimizumisaki@gmail.com>
2022-02-02 23:25:06 -0500

    Adding styling and a bit of animation :sparkles:

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Window load event
+const preloader = document.querySelector('.preloader');
+const highlightWord = document.querySelector('.highlightWord');
+const stop = 2500;
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        preloader.style.opacity = '0';
+    }, stop);
+})
