4176a4ec96baca1c902b57d9c68baa9b5eecd01c
Misaki <tshimizumisaki@gmail.com>
2022-02-03 15:58:11 -0500

    Finished the media queries :sparkles:

--- script.js
+++ script.js
@@ -6,4 +6,23 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         preloader.style.opacity = '0';
     }, stop);
+    setTimeout( () => {
+        preloader.style.display = 'none';
+    }, 2800)
 })
+// Remove transition
+const img = document.querySelectorAll('img');
+img.classList.remove('.transition');
+
+// Scroll Animation
+const observer = new IntersectionObserver( (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            img.classList.add('transition');
+            return;
+        }
+        img.classList.remove('transition');
+    })
+});
+
+observer.observe(document.querySelector('.imgWrapper'));
